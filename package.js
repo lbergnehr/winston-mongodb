e6d8da3082e086f2354df9963b9dd00ece1e630f
Leo Bergnéhr <leo@bergnehr.se>
2014-12-10 22:24:48 +0100

    Make the package actually export a variable
    And some additional syntax cleanup and minor adjustments.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'leo:winston-mongodb',
   summary: 'Meteor wrapper for the winston-mongodb npm package.',
-  version: '0.0.1'
+  version: '0.0.1',
   git: 'https://github.com/lbergnehr/winston-mongodb.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('0.9.0');
   api.export('MongoDB', 'server');
-  api.addFiles('winston-mongodb.js');
+  api.addFiles(['winston-mongodb.js'], 'server');
 });
 
 Npm.depends({
